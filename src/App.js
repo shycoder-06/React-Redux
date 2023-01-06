b834deef1b5132f20d4f83d46db0150a0bfa3248
Shyla <shailajasrinivas91@gmail.com>
2023-01-06 07:19:43 -0500

    counter accessed from redux store using useSelector

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
+	const counter = useSelector((state) => state.counter);
 	return (
 		<div>
 			<h1>Counter App</h1>
+			<h2>{counter}</h2>
 		</div>
 	);
 }
