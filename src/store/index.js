8237c031c5a761d3e005c78f8209bb129f53210a
Shyla <shailajasrinivas91@gmail.com>
2023-01-09 14:11:10 -0500

    redux toolkit: createslcie, configstore used to replace store code

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,23 @@
-import { legacy_createStore as createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { useReducer } from "react";
 
-const reducerFn = (state = { counter: 10 }, action) => {
-	//synchronous function
-	// we should not mutate the original state
-
-	if (action.type === "INC") {
-		return { counter: state.counter + 1 };
-	}
-
-	if (action.type === "DEC") {
-		return { counter: state.counter - 1 };
-	}
-
-	if (action.type === "ADD") {
-		return { counter: state.counter + action.payload };
-	}
-
-	return state;
-};
-
-const store = createStore(reducerFn);
+const counterSlice = createSlice({
+	name: "counter",
+	initialState: { counter: 0 },
+	reducers: {
+		increment(state, action) {
+			state.counter++;
+		},
+		decrement(state, action) {
+			state.counter--;
+		},
+		addBy(state, action) {
+			state.counter += action.payload;
+		},
+	},
+});
+export const actions = counterSlice.actions;
+const store = configureStore({
+	reducer: counterSlice.reducer,
+});
 export default store;
